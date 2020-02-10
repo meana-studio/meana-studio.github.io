9144a1735361e6bab8489930d8de25bd69c4665c
Kyosuke Koizumi <kyosuke@bootscafe.jp>
2020-02-10 12:28:15 +0900

    コンテンツ追加

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,16 +1,8 @@
-var mySwiper = new Swiper ('.swiper-container', {
-	loop: true,
-	slidesPerView: 1,
-	speed: 600,
-	autoplay: {
-		delay: 3000,
-	},
-	fadeEffect: {
-		crossFade: true
-	},
-	spaceBetween: 0,
-	centeredSlides : false,
-    scrollbar: {
-		el: '.swiper-scrollbar',
-	},
-  })
+window.onload = function () {
+	//initialize swiper when document ready
+	var mySwiper = new Swiper ('.swiper-container', {
+	  // Optional parameters
+	  direction: 'vertical',
+	  loop: true
+	})
+}

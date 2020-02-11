a9d98de7629588a0eebb4c8977220565f6082810
Kyosuke Koizumi <kyosuke@bootscafe.jp>
2020-02-11 09:41:51 +0900

    コンテンツ追加

--- src/js/swiper.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-	//initialize swiper when document ready
-	var mySwiper = new Swiper ('.swiper-container', {
-	  // Optional parameters
-	  direction: 'vertical',
-	  loop: true
-	})
-}

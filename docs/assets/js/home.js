556245043c009549deefe60a1a90a4ce9e3c9e3d
Kyosuke Koizumi <kyosuke@bootscafe.jp>
2024-10-04 11:08:20 +0900

    いろいろ調整

--- docs/assets/js/home.js
+++ docs/assets/js/home.js
@@ -1,1 +1,48 @@
-"use strict";var el=document.getElementById("main-cover-image"),message=document.getElementById("message"),scroll=document.getElementById("scroll");function fadeCover(){TweenLite.to(el,.9,{alpha:1,visibility:"visible",delay:1.7,ease:Power2.easeIn,onComplete:colorChange})}function colorChange(){el.classList.add("active"),document.body.classList.add("animation-end"),TweenLite.to(message,.8,{color:"#FFF",ease:Power2.easeIn,onComplete:appendScroll})}function appendScroll(){TweenLite.to(scroll,.8,{visibility:"visible",delay:.7,ease:Power2.easeIn})}TweenLite.set(el,{alpha:0,visibility:"hidden"}),TweenLite.set(message,{alpha:0,display:"none",ease:Power2.easeIn}),TweenLite.to(message,.5,{alpha:1,display:"block",ease:Power2.easeIn,onComplete:fadeCover}),inView("#main-cover").on("enter",(function(e){document.body.classList.add("home")})).on("exit",(function(e){document.body.classList.remove("home")}));
+const el = document.getElementById("main-cover-image"),
+	message = document.getElementById("message"),
+	scroll = document.getElementById("scroll");
+
+function fadeCover() {
+	TweenLite.to(el, .9, {
+		alpha: 1,
+		visibility: "visible",
+		delay: 1.7,
+		ease: Power2.easeIn,
+		onComplete: colorChange
+	})
+}
+
+function colorChange() {
+	el.classList.add("active"), document.body.classList.add("animation-end"),
+		TweenLite.to(message, .8, {
+			color: "#FFF",
+			ease: Power2.easeIn,
+			onComplete: appendScroll
+		})
+}
+
+function appendScroll() {
+	TweenLite.to(scroll, .8, {
+		visibility: "visible",
+		delay: .7,
+		ease: Power2.easeIn
+	})
+}
+
+TweenLite.set(el, {
+	alpha: 0,
+	visibility: "hidden"
+}), TweenLite.set(message, {
+	alpha: 0,
+	display: "none",
+	ease: Power2.easeIn
+}), TweenLite.to(message, .5, {
+	alpha: 1,
+	display: "block",
+	ease: Power2.easeIn,
+	onComplete: fadeCover
+}), inView("#main-cover").on("enter", (function(e) {
+	document.body.classList.add("home")
+})).on("exit", (function(e) {
+	document.body.classList.remove("home")
+}));
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper ('#gallery-container', {
+const mySwiper = new Swiper ('#gallery-container', {
 	loop: true,
 	slidesPerView: 2,
 	speed: 600,
@@ -19,18 +19,18 @@ var mySwiper = new Swiper ('#gallery-container', {
 	breakpoints: {
 		// when window width is >= 320px
 		320: {
-		  slidesPerView: 1,
-		  spaceBetween: 0
+      slidesPerView: 1,
+      spaceBetween: 0
 		},
 		// when window width is >= 480px
 		480: {
-		  slidesPerView: 1,
-		  spaceBetween: 0
+      slidesPerView: 1,
+      spaceBetween: 0
 		},
 		// when window width is >= 640px
 		720: {
-		  slidesPerView: 2,
-		  spaceBetween: 40
+      slidesPerView: 2,
+      spaceBetween: 40
 		}
-	  }
+	}
 })
